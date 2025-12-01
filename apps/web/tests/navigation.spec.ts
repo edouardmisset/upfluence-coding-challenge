f4353d693c012e0ae6037ed1da26be07e3f3334b
Edouard Misset <edouardmisset@gmail.com>
2025-12-01 11:37:07 +0100

    test: add navigation tests

---
+++ apps/web/tests/navigation.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test'
+
+const frameworks = [
+  {
+    name: 'React',
+    title: 'Upfluence Live Stream - React',
+  },
+  {
+    name: 'Vue',
+    title: 'Upfluence Live Stream - Vue',
+  },
+  {
+    name: 'Svelte',
+    title: 'Upfluence Live Stream - Svelte',
+  },
+]
+
+test.describe('Navigation', () => {
+  for (const framework of frameworks) {
+    test(`should navigate to ${framework.name} page and back`, async ({
+      page,
+    }) => {
+      await test.step('Start on home page', async () => {
+        await page.goto('')
+        await expect(
+          page.getByRole('heading', { name: 'Upfluence Coding Challenge' }),
+        ).toBeVisible()
+      })
+
+      await test.step(`Navigate to ${framework.name} page`, async () => {
+        await page.getByRole('link', { name: framework.name }).click()
+      })
+
+      await test.step('Check page content', async () => {
+        await expect(
+          page.getByRole('heading', { name: framework.title }),
+        ).toBeVisible()
+
+        await expect(page.getByRole('status')).toBeVisible()
+      })
+
+      await test.step('Go back home', async () => {
+        await page.getByRole('link', { name: 'Back to home' }).click()
+      })
+
+      await test.step('Check we are home', async () => {
+        await expect(
+          page.getByRole('heading', { name: 'Upfluence Coding Challenge' }),
+        ).toBeVisible()
+      })
+    })
+  }
+})
